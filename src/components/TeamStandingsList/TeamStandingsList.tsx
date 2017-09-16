5a2bf91a6ad117f8d84236c0b9abb275ec5aee14
Tyler Hogan <tybot204@gmail.com>
2017-09-15 21:29:45 -0700

    Fixed typescript references

--- src/components/TeamStandingsList/TeamStandingsList.tsx
+++ src/components/TeamStandingsList/TeamStandingsList.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import classNames from 'classnames';
+import * as classNames from 'classnames';
 
 import withStyles from 'material-ui/styles/withStyles';
+import { Theme } from 'material-ui/styles/createMuiTheme';
+
 import Avatar from 'material-ui/Avatar';
 import List from 'material-ui/List';
 import ListItem from 'material-ui/List/ListItem';
@@ -14,7 +16,7 @@ import Typography from 'material-ui/Typography';
 import AccountBoxIcon from 'material-ui-icons/AccountBox';
 import FolderIcon from 'material-ui-icons/Folder';
 
-const styles = theme => ({
+const styles = (theme: Theme) => ({
   paper: {
     margin: theme.spacing.unit
   },
