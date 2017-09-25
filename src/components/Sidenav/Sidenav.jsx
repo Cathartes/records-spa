66bc60a9a79d470f3e362610ecf3fbb240545428
Tyler Hogan <tybot204@gmail.com>
2017-09-24 22:40:17 -0700

    Moved logout to the sidenav

--- src/components/Sidenav/Sidenav.jsx
+++ src/components/Sidenav/Sidenav.jsx
@@ -1,13 +1,29 @@
 import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
+import Divider from 'material-ui/Divider';
 import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 
 import InboxIcon from 'material-ui-icons/Inbox';
 
+import { sidenavToggle } from '../../actions/sidenav';
+
 class Sidenav extends PureComponent {
   render() {
+    const { currentUser, onLogoutClick } = this.props;
     return (
       <List>
+        {currentUser && (
+          <div>
+            <ListItem>
+              <ListItemText primary={currentUser.attributes.email} />
+            </ListItem>
+
+            <Divider />
+          </div>
+        )}
+
         <ListItem button>
           <ListItemIcon>
             <InboxIcon />
@@ -42,9 +58,35 @@ class Sidenav extends PureComponent {
           </ListItemIcon>
           <ListItemText primary="Record Timeline" />
         </ListItem>
+
+        {currentUser && (
+          <div>
+            <Divider />
+
+            <Link to="/logout" className="button-link">
+              <ListItem button onClick={() => onLogoutClick()}>
+                <ListItemText primary="Log Out" />
+              </ListItem>
+            </Link>
+          </div>
+        )}
       </List>
     );
   }
 }
 
-export default Sidenav;
+const mapStateToProps = state => {
+  return {
+    currentUser: state.auth.currentUser
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onLogoutClick: () => {
+      dispatch(sidenavToggle(false));
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Sidenav);
